da2fb2557d71d7968ab05581cb8fd30deaed798d
Ben England <102834321+ben-england@users.noreply.github.com>
2024-07-01 20:58:10 +0100

    Added .gitignore file, returned token from another file

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+#Importing discord API, importing commands so users can access bot from server
 import discord
 from discord.ext import commands
-#Importing discord API, importing commands so users can access bot from server
 
+#I had to create a seperate file for my token so i could gitignore it due to information sensitivity, code is a return function called returntoken, with the token declared as a string
+from connectioninfo import returntoken
+
+#making sure discord has appropriate permissions for the code to run
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix = '!', intents=intents)
-#command prefix is !
 
+#command prefix is !
+client = commands.Bot(command_prefix = '!', intents=intents)
+
+#command to show bot is online
 @client.event
 async def on_ready():
     print("Bot is ready")
     print("----------------------")
-#command to show bot is online
-    
+
+#sample command, will prob comment out or remove later
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello")
 
-client.run('')
+#invoking function from other file
+client.run(returntoken())
