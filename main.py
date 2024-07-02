ab6ee2f2a47099fd7d462c61318024c86b916981
Ben England <102834321+ben-england@users.noreply.github.com>
2024-07-02 23:36:18 +0100

    added joining and leaving notifications using channel id

--- main.py
+++ main.py
@@ -19,13 +19,28 @@ async def on_ready():
     print("----------------------")
 
 #sample command, will prob comment out or remove later
-@client.command()
-async def hello(ctx):
-    await ctx.send("Hello")
+#@client.command()
+#async def hello(ctx):
+    #await ctx.send("Hello")
 
+#sends a private message to a user welcoming them to the server
 @client.event
 async def on_member_join(member):
     await member.send("Welcome to the Discord Server! I am Iroh, a bot designed by Benji for portfolio purposes!")
+
+#sends a message to notify everyone with the joining members id
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1061776513118257225)
+    emb=discord.Embed(title="A NEW CHALLENGER ENTERS THE FRAY",description=f"Welcome to the server! {member.mention} Thanks for joining!")
+    await channel.send(embed=emb)
+
+#sends a message to notify everyone with the leaving members id
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1061776513118257225)
+    emb=discord.Embed(title="A CHALLENGER HAS BEEN FELLED",description=f"{member.mention} has left!")
+    await channel.send(embed=emb)
 
 #invoking function from other file
 client.run(returntoken())
